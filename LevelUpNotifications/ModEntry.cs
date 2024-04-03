853d4c528ba79142c04f10feed3c71103eb0fe0c
Thomas <binarynova@gmail.com>
2024-04-02 23:00:56 -0500

    Level Up Notifications Mod

---
+++ LevelUpNotifications/ModEntry.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using StardewModdingAPI;
+using StardewValley;
+using System.Collections.Generic;
+using StardewModdingAPI.Events;
+using StardewValley.Inventories;
+using StardewValley.Menus;
+using Microsoft.Xna.Framework;
+
+namespace LevelUpNotifications
+{
+    public class ModEntry : Mod
+    {
+        private int numOfLevelUpsToday = 0;
+
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.Player.LevelChanged += Player_LevelChanged;
+            helper.Events.GameLoop.DayStarted += ResetLevelUpCounter;
+        }
+        
+        private void Player_LevelChanged(object? sender, LevelChangedEventArgs e)
+        {
+            if(e.NewLevel > e.OldLevel)
+            {
+                if(numOfLevelUpsToday > 0)
+                {
+                    string levelUpMessage = Helper.Translation.Get("level-up-notification").ToString();
+                    Game1.addHUDMessage(HUDMessage.ForCornerTextbox(levelUpMessage));
+                }
+
+                numOfLevelUpsToday++;
+            }
+        }
+
+        private void ResetLevelUpCounter(object? sender, EventArgs e)
+        {
+            numOfLevelUpsToday = 0;
+        }
+    }
+}
---
+++ LevelUpNotifications/README.md
@@ -0,0 +1,5 @@
+# Level Up Notifications
+Click here to see this mod's page on Nexusmods.com.
+
+# How It Works
+When you level up, this mod sends a message to the chat window to let you know!
